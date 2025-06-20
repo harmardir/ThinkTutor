a84eb7ba2aa055629ea21bbf0637664fc66a24e7
Harout Mardirossian <harmardir@gmail.com>
2025-06-21 00:25:45 +0300

    Add offline question queue sync on reconnect and improve UI responsiveness , add login/registration and homepage

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'ai_tutor',
     'lessons',
     'users',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/ask'
+LOGOUT_REDIRECT_URL = '/users/login/'
+LOGIN_URL = '/users/login/'
--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ai_tutor.urls')),
+    path('ask/', include('ai_tutor.urls')),
+    path('users/', include('users.urls')), 
+    path('', include('main.urls')),
 ]
